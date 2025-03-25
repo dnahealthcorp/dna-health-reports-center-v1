506dff4be134ba1db7260906810b6ddbbc1b4c31
dnahealthcorp <it@dnahealthcorp.com>
2025-03-25 12:25:06 +0400

    Update pdfGenerator.ts

--- src/lib/pdf/pdfGenerator.ts
+++ src/lib/pdf/pdfGenerator.ts
@@ -174,7 +174,7 @@ doc.text(greeting, contentMargin, 95);
       1: { cellWidth: 50 },
       2: { cellWidth: 50 }
     },
-    margin: { left: contentMargin, right: contentMargin }
+    margin: { left: contentMargin, right: 0}
   });
   
   // Add page number
