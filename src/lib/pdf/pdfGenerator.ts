9ba9c012b04e4821ac4e35722873212933595a47
dnahealthcorp <it@dnahealthcorp.com>
2025-03-25 13:52:11 +0400

    Update pdfGenerator.ts

--- src/lib/pdf/pdfGenerator.ts
+++ src/lib/pdf/pdfGenerator.ts
@@ -4,7 +4,7 @@ import autoTable from "jspdf-autotable";
 import { PatientFormData, Medication } from "@/types";
 import { calculateAge, convertToKg, calculateBMI, drawStatBox, addPageNumber } from "./pdfUtilities";
 import { addLogoToPage } from "./logoRenderer";
-import * as databaseService from "@/services/databaseService";
+import * as databaseService from "@/services/databaseService"; 
 
 /**
  * Generates the first page of the PDF with health statistics
