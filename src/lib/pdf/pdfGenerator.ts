93d8cf647d5d6aea65c60ed296c6b2172245a7c9
dnahealthcorp <it@dnahealthcorp.com>
2025-03-25 11:04:26 +0400

    Update pdfGenerator.ts

--- src/lib/pdf/pdfGenerator.ts
+++ src/lib/pdf/pdfGenerator.ts
@@ -13,8 +13,8 @@ const generateFirstPage = (doc: jsPDF, pageWidth: number, contentMargin: number,
   addLogoToPage(doc);
   
   // Add images instead of statistic boxes - centered on page
-  const imageWidth = 120;
-  const imageHeight = 60; // Set a fixed height to maintain aspect ratio
+  const imageWidth = 598;
+  const imageHeight = 388; // Set a fixed height to maintain aspect ratio
   const imageStartX = (pageWidth - imageWidth) / 2;
   
   try {
